ea02547979e53c867effc6245766682b7cce532f
Evgeniy.A <andreev.e.b@mail.ru>
2022-10-24 21:25:51 +0300

    Fixed loader's rotation;

---
+++ Assets/Sources/Scripts/UI/UIRotator.cs
@@ -0,0 +1,21 @@
+using DG.Tweening;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class UIRotator : MonoBehaviour
+    {
+        [SerializeField] private float RotationDuration = 3f;
+
+        private void Start()
+        {
+            if (gameObject != null)
+                transform.DOLocalRotate(new Vector3(0, 0, -360), RotationDuration, RotateMode.FastBeyond360)
+                            .SetRelative(true)
+                            .SetEase(Ease.Linear)
+                            .SetLink(gameObject)
+                            .SetLoops(-1, LoopType.Restart);
+
+        }
+    }
+}
