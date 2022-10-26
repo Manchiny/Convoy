2ef539470fa247bfe8bb582d1c1e78126b2d5364
Evgeniy.A <andreev.e.b@mail.ru>
2022-10-26 17:42:58 +0300

    Resizable ground with NavMesh;

--- Assets/Sources/Scripts/Levels/Ground.cs
+++ Assets/Sources/Scripts/Levels/Ground.cs
@@ -4,7 +4,24 @@ using UnityEngine.AI;
 namespace Assets.Scripts.Levels
 {
     public class Ground : MonoBehaviour
-    {
-        public NavMeshSurface _navMeshSurface;
+    {    
+        [SerializeField] private NavMeshSurface _navMeshSurface;
+
+        private const float ExtraLenght = 5f;
+        private const float ScaleDivider = 10f;
+
+        public void Resize(float roadSize)
+        {
+            Vector3 scale = transform.localScale;
+            scale.z = ExtraLenght + (roadSize / ScaleDivider);
+
+            Vector3 position = Vector3.zero;
+            position.z = roadSize / 2f - RoadPart.Lenght / 2f;
+
+            transform.localScale = scale;
+            transform.position = position;
+
+            _navMeshSurface.BuildNavMesh();
+        }
     }
 }
