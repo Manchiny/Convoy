d364cd26d77018d0978b38159fe2db58cdaec323
Evgeniy.A <andreev.e.b@mail.ru>
2022-10-26 17:20:18 +0300

    NavMeshComponents;

--- Assets/Sources/Scripts/Levels/Ground.cs
+++ Assets/Sources/Scripts/Levels/Ground.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Assets.Scripts.Levels
 {
     public class Ground : MonoBehaviour
     {
-
+        public NavMeshSurface _navMeshSurface;
     }
 }
